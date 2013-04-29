cea2f1b326246e567e4e650671aef866148116f2
OmegaMacMini <Air_i7@carlosazs-mac-mini.local>
2013-04-29 19:19:11 -0300

    img xhr

---
+++ Chrome/PXA/app/main.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://www.areah.com.br/images/fotos/mh_home_capa_detroicelectric.jpg', true);
+xhr.responseType = 'blob';
+xhr.onload = function(e) {
+  var img = document.createElement('img');
+  img.src = window.webkitURL.createObjectURL(this.response);
+  document.body.appendChild(img);
+};
+
+xhr.send();
